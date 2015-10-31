9b090f3f64b6afab47709633785fa84ac1c604ea
polidog <polidogs@gmail.com>
2015-10-31 16:00:28 +0900

    run.shディレクトリ指定の修正

--- es/run.sh
+++ es/run.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-docker run -d --name oresama1 -v ./config/oresama1:/usr/share/elasticsearch/config \
-                              -v ./oresama1:/usr/share/elasticsearch/data \
+
+
+docker run -d --name oresama1 -v "$PWD/config/oresama1":/usr/share/elasticsearch/config \
+                              -v /var/esdata/oresama1:/usr/share/elasticsearch/data \
                               -e "ES_HEAP_SIZE=256m" \
                               -p 9200:9200 oresama_es:latest
-docker run -d --name oresama2 -v ./config/oresama2:/usr/share/elasticsearch/config \
-                              -v ./esdata/oresama2:/usr/share/elasticsearch/data \
+docker run -d --name oresama2 -v "$PWD/config/oresama2":/usr/share/elasticsearch/config \
+                              -v /var/esdata/oresama2:/usr/share/elasticsearch/data \
                               -e "ES_HEAP_SIZE=256m" \
                               -p 9201:9200 oresama_es:latest
-docker run -d --name anatasama -v ./config/anatasama1:/usr/share/elasticsearch/config \
-                               -v ./anatasama1:/usr/share/elasticsearch/data \
+docker run -d --name anatasama -v "$PWD/config/anatasama1":/usr/share/elasticsearch/config \
+                               -v /var/esdata/anatasama1:/usr/share/elasticsearch/data \
                                -e "ES_HEAP_SIZE=256m" \
                                -p 9202:9200 oresama_es:latest
